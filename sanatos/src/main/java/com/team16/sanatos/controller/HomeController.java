05373a12f9f60e3dc90e97e917db664c6f6830d5
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2023-10-10 22:38:16 +0300

    started work on controllers

---
+++ sanatos/src/main/java/com/team16/sanatos/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.team16.sanatos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // This would return the "index" template or view
+    }
+}
---
+++ sanatos/src/main/java/com/team16/sanatos/controller/LoginController.java
@@ -0,0 +1,4 @@
+package com.team16.sanatos.controller;
+
+public class LoginController {
+}
