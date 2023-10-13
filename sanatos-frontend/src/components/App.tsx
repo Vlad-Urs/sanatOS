ddd6159816bc070186db8c205ab23ee6d351008a
botezatumarius <ufobelievers27@gmail.com>
2023-10-13 16:26:21 +0300

    Front:transition component

--- sanatos-frontend/src/components/App.tsx
+++ sanatos-frontend/src/components/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../App.css'
+import Transition from './Transition'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      < Transition />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ sanatos-frontend/src/components/Transition.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Transition extends React.Component{
+    render() {
+        return (
+            <div className = "bars">
+                <div className = "bar"></div>
+                <div className = "bar"></div>
+                <div className = "bar"></div>
+                <div className = "bar"></div>
+                <div className = "bar"></div>
+            </div>
+        );
+    }
+}
+export default Transition;
