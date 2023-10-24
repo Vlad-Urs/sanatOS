106d12eddf6e08d846572aa5c6c31a4ad1d98918
danielBctr <danielbucataru69@gmail.com>
2023-10-24 21:16:58 +0300

    changes in Encryption class

--- sanatos/src/main/java/com/team16/sanatos/Security/Encryption.java
+++ sanatos/src/main/java/com/team16/sanatos/Security/Encryption.java
@@ -1,43 +1,74 @@
 package com.team16.sanatos.Security;
 
 import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import javax.crypto.Cipher;
+import java.sql.SQLException;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyPairGenerator;
+import javax.crypto.Cipher;
+import java.sql.DriverManager;
 
 public class Encryption {
+    private static final KeyPair keyPair;
 
-    public static void encryptAndInsertData(String dataToEncrypt) throws Exception {
-        KeyPair keyPair = KeyGenerator.generateKeyPair();
+    static {
+        keyPair = generateKeyPair();
+    }
 
-        // Encrypt the data with the public key
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
-        byte[] encryptedData = cipher.doFinal(dataToEncrypt.getBytes(StandardCharsets.UTF_8));
-
-        // example of usage change to our database
-        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
-             PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO your_table (encrypted_data) VALUES (?)")) {
-            preparedStatement.setBytes(1, encryptedData);
-            preparedStatement.executeUpdate();
+    private static KeyPair generateKeyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating RSA key pair", e);
         }
     }
 
-    public static String decryptData(ResultSet resultSet, String columnName) throws Exception {
-        // Retrieve the encrypted data from the database
-        byte[] encryptedData = resultSet.getBytes(columnName);
+    public static void encryptAndInsertData(String dataToEncrypt) throws SQLException {
+        try {
+            // Encrypt the data with the public key
+            byte[] encryptedData = encryptWithPublicKey(dataToEncrypt);
 
+            // Use a connection pool for managing database connections
+            try (Connection conn = getConnection();
+                 PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO your_table (encrypted_data) VALUES (?)")) {
+                preparedStatement.setBytes(1, encryptedData);
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new SQLException("Data encryption and insertion failed", e);
+        }
+    }
 
-        KeyPair keyPair = KeyGenerator.generateKeyPair();
+    public static String decryptData(ResultSet resultSet, String columnName) throws SQLException {
+        try {
+            byte[] encryptedData = resultSet.getBytes(columnName);
+            return decryptWithPrivateKey(encryptedData);
+        } catch (Exception e) {
+            throw new SQLException("Data decryption failed", e);
+        }
+    }
 
-        // Decrypt the data with the private key
+    // Implement a getConnection() method to return a database connection from a connection pool.
+    private static Connection getConnection() throws SQLException {
+        // Implement your connection pool logic here
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
+    }
+
+    private static byte[] encryptWithPublicKey(String data) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+        return cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static String decryptWithPrivateKey(byte[] encryptedData) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
         byte[] decryptedData = cipher.doFinal(encryptedData);
-
         return new String(decryptedData, StandardCharsets.UTF_8);
     }
 }
