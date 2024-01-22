c806c65828c3dd5854a88828f78be27c4cbae9cd
botezatumarius <ufobelievers27@gmail.com>
2024-01-22 19:28:39 +0200

    enabled cross-origin resource sharing

--- sanatos-backend/app.py
+++ sanatos-backend/app.py
@@ -1,12 +1,14 @@
 # app.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from models.database import db
 from models.doctor_model import Doctor
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     # Configure SQLAlchemy to use SQLite
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
     db.init_app(app)
