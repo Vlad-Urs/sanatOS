2a94e878b463dae8c59749e29fa9a0da75ffbada
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2024-01-24 21:00:49 +0200

    added patient registering

--- sanatos-backend/controllers/patient_controller.py
+++ sanatos-backend/controllers/patient_controller.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import jsonify, request
+from flask import jsonify, request, redirect
 from __main__ import app
 from models.doctor_model import Doctor
 from models.patient_model import Patient
@@ -7,6 +7,7 @@ from models.doctor_patient_relationship_model import DoctorPatientRelationship
 from models.medical_history_model import MedicalHistory
 from models.prescriptions_model import Prescription
 from app import db
+import re
 
 @app.route('/patient-<int:patient_id>', methods=['GET'])
 def get_patient_by_id(patient_id):
@@ -87,6 +88,7 @@ def get_patient_history(patient_id):
     else:
         return jsonify({"error": "Patient not found"}), 404
 
+
 @app.route('/patient-<int:patient_id>/prescriptions', methods=['GET'])
 def get_prescriptions_for_patient(patient_id):
     # Find the Patient by ID
@@ -108,5 +110,48 @@ def get_prescriptions_for_patient(patient_id):
         ]
 
         return jsonify({"patient_prescriptions": prescriptions_data}), 200
+    else:
+        return jsonify({"error": "Patient not found"}), 404
+
+
+@app.route('/patient-<int:patient_id>/register', methods=['POST'])
+def register_patient(patient_id):
+    # Find the Patient by ID
+    patient = Patient.query.get(patient_id)
+
+    if patient is not None:
+        # Parse the JSON data from the request
+        # should a be an username and a password
+        data = request.get_json()
+
+        existing_patient = Patient.query.filter_by(username=data.get('username')).first()
+        if existing_patient:
+            return jsonify({"error": "Username already taken"}), 400
+        
+        password = data.get('password')
+        if len(password)<8:
+            return jsonify({"error":"Password not long enough"}), 400
+        
+        if not any(char.isupper() for char in password):
+            return jsonify({"error":"Password must contain capitalized letters"}), 400
+        
+        if not any(char.isdigit() for char in password):
+            return jsonify({"error":"Password must contain numbers"}), 400
+        
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            return jsonify({"error":"""Password must contain at least one symbol ([!@#$%^&*(),.?":{}|<>])"""}), 400
+        
+        if ' ' in password:
+            return jsonify({"error":"Password must not contain spaces"}), 400
+        
+        # Update the patient's username and password
+        patient.username = data.get('username')
+        patient.password = data.get('password')  # Note: You should hash and salt the password in a real-world scenario
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        return redirect('/patient-<int:patient_id>')
+        
     else:
         return jsonify({"error": "Patient not found"}), 404
--- sanatos-backend/models/prescriptions_model.py
+++ sanatos-backend/models/prescriptions_model.py
@@ -1,13 +1,10 @@
 from models.database import db
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
 
 class Prescription(db.Model):
     id = db.Column('prescription_id', db.Integer, primary_key=True, autoincrement=True)
     patient_id = db.Column(db.Integer, db.ForeignKey('patient.patient_id'), nullable=False)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.doctor_id'), nullable=False)
-    patient = relationship('Patient', back_populates='prescriptions')
-    doctor = relationship('Doctor', back_populates='prescriptions')
     medication = db.Column(db.String, nullable=False)
     dosage = db.Column(db.String, nullable=False)
     duration = db.Column(db.String, nullable=False)
