cdac30bd4bd835a4f26ef67aeef831cd8f357df3
danielBctr <danielbucataru69@gmail.com>
2024-01-24 16:45:37 +0200

    change prescriptions model, add prescriptions to Doctor controller

--- sanatos-backend/models/prescriptions_model.py
+++ sanatos-backend/models/prescriptions_model.py
@@ -1,22 +1,18 @@
 from models.database import db
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 class Prescription(db.Model):
     id = db.Column('prescription_id', db.Integer, primary_key=True, autoincrement=True)
     patient_id = db.Column(db.Integer, db.ForeignKey('patient.patient_id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.doctor_id'), nullable=False)
     patient = relationship('Patient', back_populates='prescriptions')
-    doctor_name = db.Column(db.String, nullable=False)
+    doctor = relationship('Doctor', back_populates='prescriptions')
     medication = db.Column(db.String, nullable=False)
     dosage = db.Column(db.String, nullable=False)
-    instructions = db.Column(db.String, nullable=False)
-    issue_date = db.Column(db.Date, nullable=False)
-    expiration_date = db.Column(db.Date, nullable=False)
 
-    def __init__(self, patient_id, doctor_name, medication, dosage, instructions, issue_date, expiration_date):
+    def __init__(self, patient_id, doctor_id, medication, dosage):
         self.patient_id = patient_id
-        self.doctor_name = doctor_name
+        self.doctor_id = doctor_id
         self.medication = medication
         self.dosage = dosage
-        self.instructions = instructions
-        self.issue_date = issue_date
-        self.expiration_date = expiration_date
